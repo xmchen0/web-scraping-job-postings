117e930cad3249bc2c1494c7d7c61a1b702ddf6e
Kathy Chen <kathychen.social@gmail.com>
2019-06-02 05:55:57 -0400

    heroku

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.set("view engine", "handlebars");
 
 
 // Mongo with Mongoose
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/linkedinjobs";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/linkedinjobs";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
